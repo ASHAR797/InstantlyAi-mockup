5ef15d94be339f4e36f890592ec0dd737f263702
ASHAR ALI <111843979+ASHAR797@users.noreply.github.com>
2024-02-03 17:48:19 +0500

    Updated files

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
+// Swiper js
 var swiper = new Swiper(".mySwiper", {});
-
-// section 5 Accordion
-let accordion    = document.querySelector(".sec-5-accordion-container > div:nth-child(1)")
-let accordion_child_conatiner = document.querySelector(".sec-5-accordion-container>div:nth-child(2)")
-accordion   .addEventListener("click" ,  function(){
-    if(accordion_child_conatiner.style.display = "none"){
-        accordion_child_conatiner.style.display = "flex"
-    }
-    else{
-        accordion_child_conatiner.style.display = "none"
-    }
-})
 
 // Hamburger navigation 
 
-let menu  = document.querySelector(".menu")
+let menu = document.querySelector(".menu")
 let navigation = document.querySelector("#navigation")
 
-menu.addEventListener("click",function () {
-   let visibility  = navigation.getAttribute("data-visible")
-   if(visibility === "false") {
-      navigation.setAttribute("data-visible",true)
-      menu.setAttribute("aria-expanded",true)
-   }
-   else if(visibility === "true") {
-       navigation.setAttribute("data-visible",false)
-       menu.setAttribute("aria-expanded",false)
- }
+menu.addEventListener("click", function () {
+    let visibility = navigation.getAttribute("data-visible")
+    if (visibility === "false") {
+        navigation.setAttribute("data-visible", true)
+        menu.setAttribute("aria-expanded", true)
+    }
+    else if (visibility === "true") {
+        navigation.setAttribute("data-visible", false)
+        menu.setAttribute("aria-expanded", false)
+    }
 })
+
+// Gsap Animation
+let tl = gsap.timeline()
+tl.from(".sec-1>div>div:nth-child(1) , .sec-1>div>div:nth-child(2)", {
+    duration: 1,
+    y: 160,
+    opacity: 0,
+    stagger: 0.5,
+})
+
+tl.from(".sec-2 div:nth-child(1) img:nth-child(6)", {
+    scale: 0,
+    delay: 1,
+    duration: 1,
+    rotate: 360 ,
+    scrollTrigger: ".sec-2",
+}).from(".sec-2 div:nth-child(1) img:nth-child(3)", {
+    scale: 0,
+    delay: 1,
+    duration: 1,
+    rotate: 360 ,
+    scrollTrigger: ".sec-2",
+})
+
